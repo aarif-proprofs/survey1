c3fe3d168f217466bb55b19814c5756071175e7d
Aarif <aarif@proprofs.com>
2021-12-16 12:17:16 +0530

    add devise

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,8 @@ class ArticlesController < ApplicationController
 	def index
 		puts 'hi'
 	end
+
+	def show
+	  puts 'show'
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root "articles#index"
   get "/articles", to: "articles#index"
+  resources :articles
+  devise_for :users, :controllers => { registrations: 'registrations'}
 end
